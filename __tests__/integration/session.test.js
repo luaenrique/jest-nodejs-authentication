c2ec022872ff4e8461f86abe89f46c560812d58b
Luã Enrique Zangrande <lua.enriquezan@gmail.com>
2019-11-03 22:26:07 -0300

    writing tests

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -2,23 +2,108 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 const { User } = require('../../src/app/models');
-
+const truncate = require('../utils/truncate');
 
 describe("Authentication", () => {
+    beforeEach(async () => {
+        await truncate();
+    });
+
+
    it("should authenticate with valid credentials", async() => {
        const user = await User.create({
             name: "Luã Enrique",
             email: "lua.enrique@gmail.com",
-            password_hash: "12312312",
+            password: "1234",
        });
 
        const response = await request(app)
        .post('/sessions')
        .send({
            email: user.email,
-           password: '12312321213123123123'
+           password: '1234'
        })
 
        expect(response.status).toBe(200);
    }) 
+
+   it("should have a registered email to authenticate", async() => {
+        const user = await User.create({
+            name: "Luã Enrique",
+            email: "lua.enrique@gmail.com",
+            password: "1234",
+        });
+
+        const response = await request(app)
+        .post('/sessions')
+        .send({
+            email: "asdasd@gmail.com",
+            password: '1234'
+        })
+
+        expect(response.status).toBe(401);
+    }) 
+
+   it("should not authenticate with invalid credentials", async() => {
+        const user = await User.create({
+            name: "Luã Enrique",
+            email: "lua.enrique@gmail.com",
+            password: "1234",
+        });
+
+        const response = await request(app)
+        .post('/sessions')
+        .send({
+            email: user.email,
+            password: '4321'
+        })
+
+        expect(response.status).toBe(401);
+    }) 
+
+    it("should return jwt token when authenticated", async() => {
+        const user = await User.create({
+            name: "Luã Enrique",
+            email: "lua.enrique@gmail.com",
+            password: "1234",
+        });
+
+        const response = await request(app)
+        .post('/sessions')
+        .send({
+            email: "lua.enrique@gmail.com",
+            password: '1234'
+        })
+
+        expect(response.body).toHaveProperty("token");
+    }) 
+
+    it("should be able to access private routes when authenticated", async () => {
+        const user = await User.create({
+            name: "Luã Enrique",
+            email: "lua.enrique@gmail.com",
+            password: "1234",
+        });
+    
+        const response = await request(app)
+          .get("/dashboard")
+          .set("Authorization", `Bearer ${user.generateToken()}`);
+    
+        expect(response.status).toBe(200);
+      });
+
+    it("should not be able to access private routes without jwt token", async () => {
+        const response = await request(app).get("/dashboard");
+
+        expect(response.status).toBe(401);
+    });
+
+    it("should not be able to access private routes with invalid jwt token", async () => {
+    const response = await request(app)
+        .get("/dashboard")
+        .set("Authorization", `Bearer 123123`);
+
+        expect(response.status).toBe(401);
+    });
+
 });
